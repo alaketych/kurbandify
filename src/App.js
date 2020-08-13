2f837214d276405e06164136f0b7e46ad004aa14
alaketych <alaketych@ukr.net>
2020-08-13 21:05:35 +0300

    sass styles created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import logo from './assets/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo">
+          <img className="header__logo" src={logo} alt="kurbandify-logo"></img>
+          <h1 className="header__name">kurbandify</h1>
+        </div>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
