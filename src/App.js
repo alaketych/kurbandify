53a1eff26540ab6571f22d18abe3d1e8560a7f73
alaketych <alaketych@ukr.net>
2020-08-14 16:18:48 +0300

    index html file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import logo from './assets/logo.png'
+import { BrowserRouter, Switch } from 'react-router-dom'
+import { Head, List } from './components/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <div className="logo">
-          <img className="header__logo" src={logo} alt="kurbandify-logo"></img>
-          <h1 className="header__name">kurbandify</h1>
-        </div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Head />
+
+        <Switch>
+            <List />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
