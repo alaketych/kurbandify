5463f70bc1a6eafdcdb487a0c73e847f74aef42e
alaketych <alaketych@ukr.net>
2020-08-16 15:37:46 +0300

    init point react app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Switch } from 'react-router-dom'
-import { Head, List } from './components/index'
+import { Spinner, Head } from './components/index'
+
+const Home = lazy(_ => import('./components/Home'))
+const ArtistPage = lazy(_ => import('./components/ArtistPage'))
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Head />
 
-        <Switch>
-            <List />
-        </Switch>
-      </div>
-    </BrowserRouter>
+        <div className="App">
+          <Suspense fallback={ <Spinner>L O A D I N G . . .</Spinner> }>
+            <BrowserRouter>
+              <Head />
+
+              <Switch>
+                  <Home path='/' exact/>
+                  <ArtistPage path='/artist/:id' />
+              </Switch>
+
+            </BrowserRouter>
+          </Suspense>
+        </div>
   );
 }
 
