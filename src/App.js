30b5fb040832f8527457226bf9251ded58e73dd1
alaketych <alaketych@ukr.net>
2020-08-20 22:04:04 +0300

    fixed suspense loader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Suspense, lazy } from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Spinner, Head } from './components/index'
 
-const Home = lazy(_ => import('./components/Home'))
-const ArtistPage = lazy(_ => import('./components/ArtistPage'))
+const Home = lazy(() => {
+  return new Promise(resolve => setTimeout(resolve, 1000)).then(
+    () => import('./components/Home')
+  )
+})
+
+const ArtistPage = lazy(() => {
+  return new Promise(resolve => setTimeout(resolve, 1000)).then(
+    () => import('./components/ArtistPage')
+  )
+})
+
+const AlbumPage = lazy(() => {
+  return new Promise(resolve => setTimeout(resolve, 1000)).then(
+    () => import('./components/AlbumPage')
+  )
+})
 
 function App() {
   return (
+    <div className="App">
+        <BrowserRouter>
+          <Head />
 
-        <div className="App">
-          <Suspense fallback={ <Spinner>L O A D I N G . . .</Spinner> }>
-            <BrowserRouter>
-              <Head />
-
-              <Switch>
-                  <Home path='/' exact/>
-                  <ArtistPage path='/artist/:id' />
-              </Switch>
-
-            </BrowserRouter>
+          <Suspense fallback={ <Spinner size="large" /> }>
+            <Switch>
+              <Route path="/" component={ Home } exact/>
+              <Route path="/album/:id" component={ AlbumPage }/>
+              <Route path="/artist/:id" component={ ArtistPage }/>
+            </Switch>
           </Suspense>
-        </div>
+
+        </BrowserRouter>
+    </div>
   );
 }
 
